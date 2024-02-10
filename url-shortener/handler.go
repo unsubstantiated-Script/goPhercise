ccee23270268c9693a6e1266eb6acede1cc6f435
unsubstantiated-Script <jusfulton@gmail.com>
2024-02-10 12:24:55 -0800

    wrapping up URL shortener/redirect engine

---
+++ url-shortener/handler.go
@@ -0,0 +1,78 @@
+package url_shortener
+
+import (
+	"gopkg.in/yaml.v2"
+	"net/http"
+)
+
+// MapHandler will return an http.HandlerFunc (which also implements http.Handler) that will attempt to map any paths
+// (keys in the map) to their corresponding URL (values that each key in the map points to, in string format). If the path is not provided in the map, then the fallback http.Handler will be called instead.
+func MapHandler(pathsToURLs map[string]string, fallback http.Handler) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		//if we can match a path -> redirect to it -> else create a fallback
+		path := r.URL.Path
+		if dest, ok := pathsToURLs[path]; ok {
+			http.Redirect(w, r, dest, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(w, r)
+	}
+
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//   - path: /some-path
+//     url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+func YAMLHandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	//1. Parse yaml
+	pathUrls, err := parseYaml(yamlBytes)
+
+	if err != nil {
+		return nil, err
+	}
+
+	//2. Convert yaml array into map
+	pathsToUrls := buildMap(pathUrls)
+
+	//3. return a map handler using the map
+	return MapHandler(pathsToUrls, fallback), nil
+}
+
+func buildMap(pathUrls []pathURL) map[string]string {
+	pathsToUrls := make(map[string]string)
+
+	//Looping over the pathURLs array of structs to loadup key/value pairs in the pathToUrls map.
+	for _, pu := range pathUrls {
+		pathsToUrls[pu.Path] = pu.URL
+	}
+	return pathsToUrls
+}
+
+// parseYaml parses out the YAML script for our url shortener script
+func parseYaml(data []byte) ([]pathURL, error) {
+	var pathUrls []pathURL
+	err := yaml.Unmarshal(data, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+	return pathUrls, nil
+}
+
+type pathURL struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
