087924f9db8c12629f2d22b3d588a63f908bbb46
unsubstantiated-Script <jusfulton@gmail.com>
2024-02-12 20:12:52 -0800

    Styles added

--- cya-game/story.go
+++ cya-game/story.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"html/template"
 	"io"
+	"log"
 	"net/http"
 	"strings"
 )
@@ -20,8 +21,10 @@ var defaultHandlerTemplate = `
 <head>
     <meta charset="UTF-8">
     <title>Choose You Own GoVenture</title>
+
 </head>
 <body>
+<section class="page">
     <h1>
         {{.Title}}
     </h1>
@@ -33,6 +36,47 @@ var defaultHandlerTemplate = `
             <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
         {{end}}
     </ul>
+</section>
+	<style>
+		  body {
+			font-family: helvetica, arial;
+		  }
+		  h1 {
+			text-align:center;
+			position:relative;
+		  }
+		  .page {
+			width: 80%;
+			max-width: 500px;
+			margin: auto;
+			margin-top: 40px;
+			margin-bottom: 40px;
+			padding: 80px;
+			background: #FCF6FC;
+			border: 1px solid #eee;
+			box-shadow: 0 10px 6px -6px #797;
+		  }
+		  ul {
+			border-top: 1px dotted #ccc;
+			padding: 10px 0 0 0;
+			-webkit-padding-start: 0;
+		  }
+		  li {
+			padding-top: 10px;
+		  }
+		  a,
+		  a:visited {
+			text-decoration: underline;
+			color: #555;
+		  }
+		  a:active,
+		  a:hover {
+			color: #222;
+		  }
+		  p {
+			text-indent: 1em;
+		  }
+	</style>
 </body>
 </html>`
 
@@ -55,14 +99,17 @@ func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	//Filtering out the slash that comes first
 	path = path[1:]
 
+	// Passing in ["intro"]. This does check the value in the map being there.
 	if chapter, ok := h.s[path]; ok {
-		
-	}
+		err := tpl.Execute(w, chapter)
 
-	err := tpl.Execute(w, h.s["intro"])
-	if err != nil {
-		panic(err)
+		if err != nil {
+			log.Printf("%v", err)
+			http.Error(w, "Something went wrong...", http.StatusInternalServerError)
+		}
+		return
 	}
+	http.Error(w, "Chapter not found", http.StatusNotFound)
 }
 
 func JsonStory(r io.Reader) (Story, error) {
