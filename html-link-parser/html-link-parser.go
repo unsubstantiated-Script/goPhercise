72a60f7d26695c105233e7a0a8d63dcffbbd2edf
unsubstantiated-Script <jusfulton@gmail.com>
2024-02-15 22:00:49 -0800

    Link Parser setup

---
+++ html-link-parser/html-link-parser.go
@@ -0,0 +1,3 @@
+package html_link_parser
+
+func RollHTMLLinkParser() {}
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
-import cya_game "goPhercise/cya-game"
+import (
+	html_link_parser "goPhercise/html-link-parser"
+)
 
 func main() {
 	//quiz_game.RollQuizGame()
 	//url_shortener.RollURLShortener()
-	cya_game.RollCYAGame()
+	//cya_game.RollCYAGame()
+	html_link_parser.RollHTMLLinkParser()
 }
