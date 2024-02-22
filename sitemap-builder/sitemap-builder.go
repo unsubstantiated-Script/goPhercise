dfa3161475a71c4891b4fd44cd90336d0f4ed73e
unsubstantiated-Script <jusfulton@gmail.com>
2024-02-21 18:07:32 -0800

    Base functionality in place

--- sitemap-builder/sitemap-builder.go
+++ sitemap-builder/sitemap-builder.go
@@ -6,6 +6,7 @@ import (
 	link "goPhercise/html-link-parser"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 /*
@@ -18,7 +19,7 @@ import (
 */
 
 func RollSitemapBuilder() {
-	urlFlag := flag.String("url", "https://gophercises.com/demos/cyoa", "The url you wanna build a sitemap for.")
+	urlFlag := flag.String("url", "https://gophercises.com", "The url you wanna build a sitemap for.")
 	flag.Parse()
 
 	fmt.Println(*urlFlag)
@@ -32,11 +33,6 @@ func RollSitemapBuilder() {
 
 	//Copies from a reader os.Stdout to a writer resp.Body
 	//io.Copy(os.Stdout, resp.Body)
-
-	links, _ := link.Parse(resp.Body)
-	for _, l := range links {
-		fmt.Println(l)
-	}
 
 	/*
 		/some-path
@@ -56,6 +52,24 @@ func RollSitemapBuilder() {
 	}
 
 	base := baseUrl.String()
-	fmt.Println("Request URL:", reqUrl.String())
-	fmt.Println("Base URL:", base)
+
+	links, _ := link.Parse(resp.Body)
+	//for _, l := range links {
+	//	fmt.Println(l)
+	//}
+
+	var hrefs []string
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			hrefs = append(hrefs, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			hrefs = append(hrefs, l.Href)
+		}
+	}
+
+	for _, href := range hrefs {
+		fmt.Println(href)
+	}
+
 }
