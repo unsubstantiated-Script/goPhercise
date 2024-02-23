4acc597472db25d3b3c97c1a582a7508d57b4a54
unsubstantiated-Script <jusfulton@gmail.com>
2024-02-23 05:29:03 -0800

    dupLines complete

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
-import aecho "goPhercise/goBook/1-A-Echo"
+import bduplines "goPhercise/goBook/1-B-DupLines"
 
 func main() {
 	//quiz_game.RollQuizGame()
 	//url_shortener.RollURLShortener()
 	//cya_game.RollCYAGame()
 	//HTML Link parser is run in its own directories
-	aecho.RollEcho()
+	//aecho.RollEcho()
+	bduplines.RollDupLines()
 	//sitemap_builder.RollSitemapBuilder()
 }
