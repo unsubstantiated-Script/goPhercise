e2bcf11ce95e825fb4a16f75e42cd96066149904
unsubstantiated-Script <jusfulton@gmail.com>
2024-02-24 08:42:27 -0800

    Fetch concurrent

--- goBook/1-D-FetchURL/fetchURLConcurrent.go
+++ goBook/1-D-FetchURL/fetchURLConcurrent.go
@@ -1,5 +1,44 @@
 package __D_FetchURL
 
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
 func RollFetchURLConcurrent() {
-	
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+		go fetch(url, ch)
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close() //No leaking!
+	if err != nil {
+		ch <- fmt.Sprintf("While reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
 }
