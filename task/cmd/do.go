5a4684656aece8702f4ea10e866e92b5bf479470
unsubstantiated-Script <jusfulton@gmail.com>
2024-03-09 08:18:35 -0800

    Comamnds complete with logic

--- task/cmd/do.go
+++ task/cmd/do.go
@@ -5,8 +5,9 @@ package cmd
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
+	"goPhercise/task/db"
+	"strconv"
 )
 
 // doCmd represents the do command
@@ -14,7 +15,39 @@ var doCmd = &cobra.Command{
 	Use:   "do",
 	Short: "Marks a task as complete.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("do called")
+		var ids []int
+		//Parsing args as ints
+		for _, arg := range args {
+			id, err := strconv.Atoi(arg)
+			if err != nil {
+				fmt.Println("Failed to parse the argument:", arg)
+			} else {
+				ids = append(ids, id)
+			}
+		}
+		//Making sure we can 1 base the task for referencing.
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("Something went wrong:", err)
+			return
+		}
+
+		for _, id := range ids {
+			//Doing it this way because it's one based.
+			if id <= 0 || id > len(tasks) {
+				fmt.Println("Invalid task number:", id)
+				continue
+			}
+			//Making it zero based again
+			task := tasks[id-1]
+
+			err := db.DeleteTask(task.Key)
+			if err != nil {
+				fmt.Printf("Failed to mark \" %d\" as completed. Error: %s\n", id, err)
+			} else {
+				fmt.Printf("Marked \"%d\" as completed.\n", id)
+			}
+		}
 	},
 }
 
--- task/cmd/list.go
+++ task/cmd/list.go
@@ -5,6 +5,8 @@ package cmd
 
 import (
 	"fmt"
+	"goPhercise/task/db"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -14,7 +16,23 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "Lists all of your tasks.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("Something went wrong:", err.Error())
+			os.Exit(1)
+		}
+
+		if len(tasks) == 0 {
+			fmt.Println("You have no tasks to complete! Great job you stud you! Now go study.")
+			return
+		}
+
+		fmt.Println("You have the following tasks:")
+
+		//index based so we want this to start at 1 not zero
+		for i, task := range tasks {
+			fmt.Printf("%d. %s, Key=%d\n", i+1, task.Value, task.Key)
+		}
 	},
 }
 
