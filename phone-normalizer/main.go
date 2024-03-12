072487377fc57e33c189ab970890fe97e2d18ed7
unsubstantiated-Script <jusfulton@gmail.com>
2024-03-11 19:57:05 -0700

    DB setup complete

--- phone-normalizer/main.go
+++ phone-normalizer/main.go
@@ -1,8 +1,71 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
 	"regexp"
 )
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "password"
+	dbname   = "gophercises_phone"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password)
+	db, err := sql.Open("postgres", psqlInfo)
+	must(err)
+
+	err = resetDB(db, dbname)
+
+	must(err)
+
+	db.Close()
+
+	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
+	db, err = sql.Open("postgres", psqlInfo)
+	must(err)
+	defer db.Close()
+
+	//must(db.Ping())
+	must(createPhoneNumbersTable(db))
+}
+
+func createPhoneNumbersTable(db *sql.DB) error {
+	statement := `
+	CREATE TABLE IF NOT EXISTS phone_numbers (
+	    id SERIAL,
+	    value VARCHAR(255)
+	    )`
+	_, err := db.Exec(statement)
+	return err
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func resetDB(db *sql.DB, name string) error {
+	_, err := db.Exec("DROP DATABASE IF EXISTS " + name)
+	if err != nil {
+		return err
+	}
+	return createDB(db, name)
+}
+
+func createDB(db *sql.DB, name string) error {
+	_, err := db.Exec("CREATE DATABASE " + name)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 // REGEX version
 func normalize(phone string) string {
