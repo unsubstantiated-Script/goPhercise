b6ce2d5bb21a802b0ab53739c83051310625f957
unsubstantiated-Script <jusfulton@gmail.com>
2024-03-29 11:48:58 -0700

    Base game done

--- blackjack/blackjack.go
+++ blackjack/blackjack.go
@@ -16,16 +16,101 @@ func (h Hand) String() string {
 	return strings.Join(strs, ", ")
 }
 
+func (h Hand) DealerString() string {
+	return h[0].String() + ", **HIDDEN**"
+}
+
+func (h Hand) Score() int {
+	minScore := h.MinScore()
+	if minScore > 11 {
+		return minScore
+	}
+
+	for _, c := range h {
+		if c.Rank == deck.Ace {
+			// ace is currently worth 1, and we are changing it to be worth 11
+			// 11-1=10
+			return minScore + 10
+		}
+	}
+	return minScore
+}
+
+func (h Hand) MinScore() int {
+	score := 0
+
+	for _, c := range h {
+		score += min(int(c.Rank), 10)
+	}
+
+	return score
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
 func BlackJack() {
 	cards := deck.New(deck.Deck(3), deck.Shuffle)
 	var card deck.Card
-	for i := 0; i < 10; i++ {
-		//Drawing off the first card and leaving the rest
-		card, cards = cards[0], cards[1:]
-		fmt.Println(card)
+	var player, dealer Hand
+
+	//This nested loop allows us to deal out a card 1x1 to each member of the game.
+	for i := 0; i < 2; i++ {
+		//Methods here manipulate the data vs. making a copy.
+		for _, hand := range []*Hand{&player, &dealer} {
+			//Draw a "card" and the rest of the "cards" still left in the deck.
+			card, cards = draw(cards)
+			*hand = append(*hand, card)
+		}
 	}
 
-	var h Hand = cards[0:3]
+	var input string
 
-	fmt.Println(h)
+	//As long as the user "stands"
+	for input != "s" {
+		fmt.Println("Player:", player)
+		fmt.Println("Dealer:", dealer.DealerString())
+		fmt.Println("What will you do? (h)it, (s)tand")
+		fmt.Scanf("%s\n", &input)
+		switch input {
+		case "h":
+			card, cards = draw(cards)
+			player = append(player, card)
+		}
+	}
+
+	// Dealer logic
+	// If dealer score <= 16, we hit
+	// If dealer has a soft 17, then we hit.
+	for dealer.Score() <= 16 || (dealer.Score() == 17 && dealer.MinScore() != 17) {
+		card, cards = draw(cards)
+		dealer = append(dealer, card)
+	}
+
+	pScore, dScore := player.Score(), dealer.Score()
+	fmt.Println("==Final Hands==")
+	fmt.Println("Player:", player, "\nScore:", pScore)
+	fmt.Println("Dealer:", dealer, "\nScore:", dScore)
+
+	switch {
+	case pScore > 21:
+		fmt.Println("You busted")
+	case dScore > 21:
+		fmt.Println("Dealer busted")
+	case pScore > dScore:
+		fmt.Println("You win!")
+	case pScore < dScore:
+		fmt.Println("Dealer wins....Boooo")
+	case pScore == dScore:
+		fmt.Println("Draw")
+	}
+
+}
+
+func draw(cards []deck.Card) (deck.Card, []deck.Card) {
+	return cards[0], cards[1:]
 }
